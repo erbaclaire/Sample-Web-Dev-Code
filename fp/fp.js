0b1d55ce3c296b49ee20a9e5babe2b3e336a64b3
Claire Erba <56267696+erbaclaire@users.noreply.github.com>
2020-02-10 21:05:31 -0600

    FP Initial Submission

--- fp/fp.js
+++ fp/fp.js
@@ -1,44 +1,88 @@
-// On load of page 1) add stock symbols to drop down menu 2) load stock portfolio
-document.addEventListener("DOMContentLoaded", loadPage);
-async function loadPage(e) {
-	e.preventDefault();
-	console.log(e);
-	let http_response = await fetch("https://cloud.iexapis.com/stable/stocksUSNoUTP?token=sk_0b2fe21aa32342e7b0a1806dff4beaf6");
-	let data = await http_response.json();
-	const select = document.querySelector("select");
-	console.log(data);
-	for (symbol of data) {
-		console.log(symbol);
-		let option = document.createElement("option");
-		let text = document.createTextNode(symbol);
-		option.appendChild(text);
-		select.appendChild(option);
+// Load user's stock portfolio when pages loads
+console.log(localStorage);
+document.addEventListener("DOMContentLoaded", loadStockPortfolio);
+function loadStockPortfolio(localStorage) {
+	for (item in localStorage) {
+		
 	}
 }
 
+// When user types in stock symbol, fill in price
+async function logPrice(e) {
+	e.preventDefault();
+	console.log(e);
+  	const symbolInput = document.querySelector("#symbol");
+  	const symbolValue = symbolInput.value;
+	let http_response = await fetch("https://cloud.iexapis.com/stable/stock/market/batch?symbols=" + symbolValue + "&types=quote&token=sk_0b2fe21aa32342e7b0a1806dff4beaf6");
+	let data = await http_response.json();
+	console.log(data);
+	let pps = document.querySelector("#pps");
+	pps.value = data[symbolValue].quote.latestPrice
+}
+document.addEventListener("input", logPrice);
+
+// When user enters the shares they want, fill in the total
+async function logTotal(e) {
+	e.preventDefault();
+	console.log(e);
+  	const sharesInput = document.querySelector("#shares");
+  	const sharesValue = sharesInput.value;
+  	const totalCost = document.querySelector("#total");
+  	const pps = document.querySelector("#pps");
+  	totalCost.value = pps.value*sharesValue
+}
+document.addEventListener("input", logTotal);
 
 // Purchase stock and add it to the stock portfolio
 function buyStock(e) {
-	e.preventDefault;
+	e.preventDefault();
 	console.log(e);
-	const symbol = document.querySelector("#symbol");
-	const shares = document.querySelector("#shares");
-	const total = document.querySelector("#total");
+	const symbolInput = document.querySelector("#symbol");
+	const symbolValue = symbolInput.value;
+	const ppsInput = document.querySelector("#pps");
+	const ppsValue = ppsInput.value;
+	const sharesInput = document.querySelector("#shares");
+	const sharesValue = sharesInput.value;
+	const totalInput = document.querySelector("#total");
+	const totalValue = totalInput.value;
 	let stocks = document.querySelector("#stocks tbody");
 
+	// If some of stock is not already purchased
 	let newRow = document.createElement("tr");
 	let newCol = document.createElement("td");
-	let newSymbol = document.createTextNode(symbol);
+	let newSymbol = document.createTextNode(symbolValue);
 	newCol.appendChild(newSymbol);
 	newRow.appendChild(newCol);
+
 	let newCol2 = document.createElement("td");
-	let newShares = document.createTextNode(shares);
+	let newShares = document.createTextNode(sharesValue);
 	newCol2.appendChild(newShares);
-	newRow.appendChild(newCol);
+	newRow.appendChild(newCol2);
+
 	let newCol3 = document.createElement("td");
-	let newTotal = document.createTextNode(total);
-	newCol3.appendChild(newShares);
-	newRow.appendChild(newCol);
+	let newPrice = document.createTextNode("$" + ppsValue);
+	newCol3.appendChild(newPrice);
+	newRow.appendChild(newCol3);	
+
+	let newCol4 = document.createElement("td");
+	let newTotal = document.createTextNode("$" + totalValue);
+	newCol4.appendChild(newTotal);
+	newRow.appendChild(newCol4);
+
+	let newCol5 = document.createElement("td");
+	let newInitial = document.createTextNode("$" + totalValue);
+	newCol5.appendChild(newInitial);
+	newRow.appendChild(newCol5);
+
+	let newCol6 = document.createElement("td");
+	let newPL = document.createTextNode("$" + 0);
+	newCol6.appendChild(newPL);
+	newCol6.style.color = "green";
+	newRow.appendChild(newCol6);
+
 	stocks.appendChild(newRow);
+
+	var portfolioData = [sharesValue, totalValue];
+	localStorage.setItem(symbolValue, JSON.stringify(portfolioData));
 }
-document.querySelector("#buy").addEventListener("submit", buyStock);
+document.querySelector("#purchase-stock").addEventListener("submit", buyStock);
